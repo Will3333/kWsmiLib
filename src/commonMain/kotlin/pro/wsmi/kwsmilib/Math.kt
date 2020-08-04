cf6a431ed9f9db9bcfa9847c841896d6e2e5b925
William Smith <william.smith@wsmi.pro>
2020-08-04 23:56:29 +0200

    Correction limit function

--- src/commonMain/kotlin/pro/wsmi/kwsmilib/Math.kt
+++ src/commonMain/kotlin/pro/wsmi/kwsmilib/Math.kt
@@ -10,8 +10,8 @@
 
 package pro.wsmi.kwsmilib
 
-fun <T> Comparable<T>.limit(upper_limit: Comparable<T>? = null, lower_limit: Comparable<T>? = null) : Comparable<T> = when {
-    upper_limit != null && this > (upper_limit as T) -> upper_limit
-    lower_limit != null && this < (lower_limit as T) -> lower_limit
+fun Double.limit(upper_limit: Double? = null, lower_limit: Double? = null) : Double = when {
+    upper_limit != null && this > upper_limit -> upper_limit
+    lower_limit != null && this < lower_limit -> lower_limit
     else -> this
 }
