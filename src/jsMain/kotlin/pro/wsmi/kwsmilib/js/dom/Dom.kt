6e6526a43461c9304851072e5cb16cc6fcdb80ac
William Smith <william.smith@wsmi.pro>
2020-09-23 21:23:56 +0200

    - Changing function name from removeAllElementsByClassName to removeAllElementsWithClassName - Adding function removeAllElementsWithClassNames

--- src/jsMain/kotlin/pro/wsmi/kwsmilib/js/dom/Dom.kt
+++ src/jsMain/kotlin/pro/wsmi/kwsmilib/js/dom/Dom.kt
@@ -3,9 +3,15 @@ package pro.wsmi.kwsmilib.js.dom
 import org.w3c.dom.Document
 import org.w3c.dom.get
 
-fun Document.removeAllElementsByClassName(className: String) {
+fun Document.removeAllElementsWithClassName(className: String) {
     this.getElementsByClassName(className).let {
         while (it.length > 0)
             it[0]?.remove()
+    }
+}
+
+fun Document.removeAllElementsWithClassNames(classNames: List<String>) {
+    classNames.forEach {
+        this.removeAllElementsWithClassName(it)
     }
 }
